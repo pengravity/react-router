c48655416b2c3dfa989b9874f34c38f89c55c03f
Andy <alipinski@mail.com>
2022-10-24 09:23:49 +0200

    display blog details

--- src/components/router/BlogDetails.js
+++ src/components/router/BlogDetails.js
@@ -3,11 +3,32 @@ import { Link, useParams } from 'react-router-dom';
 import { blogs } from './data';
 
 const BlogDetails = () => {
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const [author, setAuthor] = useState('');
+
   const { id } = useParams();
+
+  useEffect(() => {
+    const thisBlog = blogs.find((blog) => blog.id === parseInt(id));
+
+    setTitle(thisBlog.title);
+    setDetails(thisBlog.details);
+    setAuthor(thisBlog.author);
+  });
+
   return (
     <div className='container --p'>
-      <h1 className='--text-center'>Blog Details {id}</h1>
-      <p>Welcome to the blog details page.</p>
+      <h1 className='--text-center'>{title}</h1>
+      <hr />
+      <p>
+        <b>Author: {author}</b>
+      </p>
+      <br />
+      <p>{details}</p>
+      <div className='--my2'>
+        <Link to='/blogs'>{`<<< Back to Blogs`}</Link>
+      </div>
     </div>
   );
 };
